d80dbc4ae02a2dc19928ce2c90f674558d61dcb7
alumno <alumno@A04CLI177>
2015-05-11 21:34:40 +0200

    Bugs - start views

--- src/java/bean/BeanHospital.java
+++ src/java/bean/BeanHospital.java
@@ -19,8 +19,13 @@ public class BeanHospital implements java.io.Serializable{
     private boolean seleccion;
     private HospitalBO hospitalBO;
     
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+  
     
-    
+                
     public void seleccionLista(){
         Hospital h = (Hospital)tabla.getRowData();
         this.seleccion = true;
@@ -55,5 +60,35 @@ public class BeanHospital implements java.io.Serializable{
         this.seleccion = seleccion;
     }
     /****************** ************************/
-    
+      public HospitalBO getHospitalBO() {
+        return hospitalBO;
+    }
+
+    public void setHospitalBO(HospitalBO hospitalBO) {
+        this.hospitalBO = hospitalBO;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
